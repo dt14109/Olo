4354c3cf580a595e35aecdd79b425bf09070d885
Darrin <dt14109@gmail.com>
2019-03-28 12:49:18 -0500

    Adding all files

---
+++ Olo/Olo/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Olo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.buttonGet = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxFile = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(711, 394);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 25);
+            this.buttonClose.TabIndex = 6;
+            this.buttonClose.Text = "&Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // buttonGet
+            // 
+            this.buttonGet.Location = new System.Drawing.Point(612, 394);
+            this.buttonGet.Name = "buttonGet";
+            this.buttonGet.Size = new System.Drawing.Size(89, 25);
+            this.buttonGet.TabIndex = 5;
+            this.buttonGet.Text = "&Get Toppings";
+            this.buttonGet.UseVisualStyleBackColor = true;
+            this.buttonGet.Click += new System.EventHandler(this.buttonRead_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "&File to read:";
+            // 
+            // textBoxFile
+            // 
+            this.textBoxFile.Location = new System.Drawing.Point(81, 25);
+            this.textBoxFile.Name = "textBoxFile";
+            this.textBoxFile.Size = new System.Drawing.Size(297, 20);
+            this.textBoxFile.TabIndex = 1;
+            this.textBoxFile.Text = "http://files.olo.com/pizzas.json ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(401, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "&Number of Toppings:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Rank  Number ordered                   Toppings";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 70);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(363, 275);
+            this.listBox1.TabIndex = 4;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(526, 25);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(63, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonGet);
+            this.Controls.Add(this.buttonClose);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Button buttonGet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Olo/Olo/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Web.Script.Serialization;
+using System.Linq;
+
+namespace Olo
+{
+    /**
+     * This class contains all our Toppings
+     */
+    public class Topping
+    {
+        private string[] tops;
+
+        public string[] toppings {
+            get
+            {
+                return this.tops;
+            }
+            // We have to sort the toppings otherwise "peppers, olives" would be different than "olives, peppers"
+            set
+            {
+                this.tops = value;
+                Array.Sort(this.tops);
+            }
+        }
+
+        /**
+         * Return a string of all toppings joined together, for example "olives peppers"
+         * We will need this for comparison
+         * 
+         */
+        public string getAllData()
+        {
+            string data = "";
+            foreach(var temp in this.tops)
+            {
+                data += temp;
+                data += " ";
+            }
+            return data;
+        }
+    }
+
+    /**
+     * The Form and processing of the data
+     */
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /**
+         * Pushed the button
+         */
+        private void buttonRead_Click(object sender, EventArgs e)
+        {
+            string file = this.textBoxFile.Text;// The file to read
+            string buffer;
+            decimal items = 0;
+            KeyValuePair<string, long> kvp;
+            WebClient client = new WebClient();
+            Stream stream = null;
+
+            try
+            {
+                stream = client.OpenRead(file);
+            }
+            catch (WebException ex)
+            {
+                MessageBox.Show("Seems we had an error: " + ex.Message);
+                return;
+            }
+            
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                // Each item is read into a Topping class
+                buffer = reader.ReadToEnd();
+                var toppings = new JavaScriptSerializer().Deserialize<List<Topping>>(buffer);
+
+                // Each Topping class is now sorted alphabetically.  So we need a count of each topping(s).  
+                // We will create a Dictionary<key, value> where the key are the toppings joined together 
+                // in one string and the value is the number of times we have those toppings
+                Dictionary<string, long> dictionary = new Dictionary<string, long>();
+                foreach(Topping topping in toppings)
+                {
+                    buffer = topping.getAllData();
+                    if (dictionary.ContainsKey(buffer))
+                    {
+                        long l = dictionary[buffer];
+                        l++;
+                        dictionary.Remove(buffer); // Remove it before putting it back in
+                        dictionary.Add(buffer, l);
+                    }
+                    else
+                    {
+                        // This is the first time these toppings are added
+                        dictionary.Add(buffer, 1);
+                    }
+                }
+
+                // Sort the dictionary by values so that we have them in order 
+                var orderedDictionary = dictionary.OrderByDescending(x => x.Value);
+
+                // How many items do we want to display?  20 by default
+                // But what if there are only 15 items?
+                if (orderedDictionary.Count() < this.numericUpDown1.Value)
+                {
+                    MessageBox.Show("Error", "You are asking for " + this.numericUpDown1.Value + " items.  But there are only " + orderedDictionary.Count() + " items.");
+                    items = orderedDictionary.Count();
+                }
+                else
+                {
+                    items = this.numericUpDown1.Value;
+                }
+
+                // Display order: rank(most popular at top)  number of times ordered  toppings  
+                for (int i = 0; i < items; i++)
+                {
+                    kvp = orderedDictionary.ElementAt(i);
+                    buffer = String.Format("{0}\t{1}\t\t{2}", i + 1, kvp.Value, kvp.Key);
+                    this.listBox1.Items.Add(buffer);
+                }
+            }
+        }
+
+        /**
+         * Close the window
+         */
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
